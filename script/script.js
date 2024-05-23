f87f80384e09f458e408396aa6d111b6ae5e5900
robynainsley21 <robynainsley2i@gmail.com>
2024-05-23 09:00:31 +0200

    Complete tasks

---
+++ script/script.js
@@ -0,0 +1,18 @@
+//question 1
+const greet = (word) => {
+    console.log(word)
+}
+greet('hello')
+
+//question 2
+const multiply = (num1, num2) => {
+    console.log(num1 * num2)
+}
+multiply(5, 6)
+
+//question 3
+const tableOfFive = (num) => {
+    let paramTimesFive = num * 5
+    console.log(paramTimesFive)
+}
+tableOfFive(8)
